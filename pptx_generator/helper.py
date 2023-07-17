1ca547c2c67805ac15186858fe1f3df23eab7536
maakhu <egri@me.com>
2023-07-17 19:57:54 +0200

    modularize pptx generator file and call it in helper file

--- pptx_generator/helper.py
+++ pptx_generator/helper.py
@@ -1,3 +1,5 @@
+from pptx_generator import create_report
+
 user_input_message = "Please enter the .json and .csv filenames separated by comma" \
                      " to prepare the report!" \
                      "(sample.json, sample.csv)\n"
@@ -10,7 +12,7 @@ def validate_and_execute(user_input):
             print("please enter both file names!")
         elif user_input["json"].endswith(".json") and user_input["csv"].endswith(".csv"):
             files = list(user_input.values())
-            return files
+            create_report(files)
         else:
             print("please enter only the filenames with their extensions")
 
--- pptx_generator/pptx_generator.py
+++ pptx_generator/pptx_generator.py
@@ -1,16 +1,6 @@
-# Project planning
-# make separate classes for different layout styles
-# - Title slide
-# - Text slide
-# - List slide
-# - Picture slide
-# - Plot slide (the plot data should be read from a .dat file)
-#  Each class/function should take relevant parameters and
-#  generate the corresponding slide using the python-pptx library.
-
-# Define a function to read the configuration file and generate the report based on the configuration.
 import collections
 import collections.abc
+from helper import validate_and_execute
 from pptx import Presentation
 from datetime import datetime
 from layouts.Title import TitleSlide
@@ -22,43 +12,44 @@ import json
 import csv
 
 
-filename_time = datetime.now().strftime("%H:%M:%S_%d.%m.%Y")
-title = "report_" + str(filename_time) + ".pptx"
+def create_report(files):
+    filename_time = datetime.now().strftime("%H:%M:%S_%d.%m.%Y")
+    title = "report_" + str(filename_time) + ".pptx"
 
-presentation = Presentation()
+    presentation = Presentation()
 
-title_slide = TitleSlide("My Title", "Subtitle")
-title_slide.create_slide(presentation)
+    title_slide = TitleSlide("My Title", "Subtitle")
+    title_slide.create_slide(presentation)
 
-bullets = ["Bullet 1", "Bullet 2", "Bullet 3"]
-bullet_slide = BulletSlide("Bullet Slide Title", bullets)
-bullet_slide.create_slide(presentation)
+    bullets = ["Bullet 1", "Bullet 2", "Bullet 3"]
+    bullet_slide = BulletSlide("Bullet Slide Title", bullets)
+    bullet_slide.create_slide(presentation)
 
-picture = "pic.png"
-pic_slide = PictureSlide("Picture Title", picture)
-pic_slide.create_slide(presentation)
+    picture = "pic.png"
+    pic_slide = PictureSlide("Picture Title", picture)
+    pic_slide.create_slide(presentation)
 
-text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod " \
-       "tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, " \
-       "quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea " \
-       "commodo consequat. Duis aute irure dolor in reprehenderit in voluptate " \
-       "velit esse cillum dolore eu fugiat " \
-       "nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in" \
-       "culpa qui officia deserunt mollit anim id est laborum."
+    text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod " \
+        "tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, " \
+        "quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea " \
+        "commodo consequat. Duis aute irure dolor in reprehenderit in voluptate " \
+        "velit esse cillum dolore eu fugiat " \
+        "nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in" \
+        "culpa qui officia deserunt mollit anim id est laborum."
 
-text_slide = TextSlide("Lorem Ipsum", text)
-text_slide.create_slide(presentation)
+    text_slide = TextSlide("Lorem Ipsum", text)
+    text_slide.create_slide(presentation)
 
-# Read plot data from the content file
-content = sample.csv
-with open(content) as file:
-       plot_data = json.load(file)
+    # Read plot data from the content file
+    content = files[1]
+    with open(content) as file:
+        plot_data = json.load(file)
 
-plot = {
+    plot = {
        "x-label": "The Plot X Label",
        "y-label": "The Plot Y Label"
-}
-plot_slide = PlotSlide("Title", plot_data, plot)
-plot_slide.create_slide(presentation)
+    }
+    plot_slide = PlotSlide("Title", plot_data, plot)
+    plot_slide.create_slide(presentation)
 
-presentation.save(title)
+    presentation.save(title)
