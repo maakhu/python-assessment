225889bed57b092d3fbd5a31c793deb353b12f76
maakhu <egri@me.com>
2023-07-17 21:00:49 +0200

    add if logic to pptx generator and call moduls

--- pptx_generator/pptx_generator.py
+++ pptx_generator/pptx_generator.py
@@ -1,6 +1,5 @@
 import collections
 import collections.abc
-from helper import validate_and_execute
 from pptx import Presentation
 from datetime import datetime
 from layouts.Title import TitleSlide
@@ -14,42 +13,58 @@ import csv
 
 def create_report(files):
     filename_time = datetime.now().strftime("%H:%M:%S_%d.%m.%Y")
-    title = "report_" + str(filename_time) + ".pptx"
+    filename = "report_" + str(filename_time) + ".pptx"
+
+    # Read input files
+    json_file_location = files[0]
+    chart_data_location = files[1]
+
+    json_content = open(json_file_location)
+    json_data = json.load(json_content)
 
     presentation = Presentation()
 
-    title_slide = TitleSlide("My Title", "Subtitle")
+    for slide in json_data['presentation']:
+        if slide["type"] == "title":
+            titles(presentation, slide['title'], slide['content'])
+        elif slide["type"] == "text":
+            text(presentation, slide['title'], slide['content'])
+        elif slide["type"] == "list":
+            bullet(presentation, slide['title'], slide['content'])
+        elif slide["type"] == "picture":
+            pic(presentation, slide['title'], slide['content'])
+        elif slide["type"] == "plot":
+            print("plot")
+
+    presentation.save(filename)
+
+
+def titles(presentation, title, content):
+    title_slide = TitleSlide(title, content)
     title_slide.create_slide(presentation)
 
-    bullets = ["Bullet 1", "Bullet 2", "Bullet 3"]
-    bullet_slide = BulletSlide("Bullet Slide Title", bullets)
+
+def bullet(presentation, title, content):
+    bullets = content
+    bullet_slide = BulletSlide(title, bullets)
     bullet_slide.create_slide(presentation)
 
-    picture = "pic.png"
-    pic_slide = PictureSlide("Picture Title", picture)
+
+def pic(presentation, title, content):
+    pic_slide = PictureSlide(title, content)
     pic_slide.create_slide(presentation)
 
-    text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod " \
-        "tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, " \
-        "quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea " \
-        "commodo consequat. Duis aute irure dolor in reprehenderit in voluptate " \
-        "velit esse cillum dolore eu fugiat " \
-        "nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in" \
-        "culpa qui officia deserunt mollit anim id est laborum."
 
-    text_slide = TextSlide("Lorem Ipsum", text)
+def text(presentation, title, content):
+    text_slide = TextSlide(title, content)
     text_slide.create_slide(presentation)
 
-    # Read plot data from the content file
-    content = files[1]
-    with open(content) as file:
-        plot_data = json.load(file)
 
+def plot(presentation, title, content, configuration):
     plot = {
        "x-label": "The Plot X Label",
        "y-label": "The Plot Y Label"
     }
-    plot_slide = PlotSlide("Title", plot_data, plot)
+    plot_slide = PlotSlide("Title", "plot_data", plot)
     plot_slide.create_slide(presentation)
 
-    presentation.save(title)
