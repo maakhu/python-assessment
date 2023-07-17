fbcb7a341fce704da563747c254732ba0c25b12f
maakhu <egri@me.com>
2023-07-17 10:28:07 +0200

    add bullet layout class

---
+++ layouts/Bullet.py
@@ -0,0 +1,26 @@
+class BulletSlide:
+    def __init__(self, title, bullets):
+        self.title = title
+        self.bullets = bullets
+
+    def create_slide(self, presentation):
+        bullet_slide_layout = presentation.slide_layouts[1]
+        slide = presentation.slides.add_slide(bullet_slide_layout)
+        shapes = slide.shapes
+
+        title_shape = shapes.title
+        body_shape = shapes.placeholders[1]
+
+        title_shape.text = 'Adding a Bullet Slide'
+
+        tf = body_shape.text_frame
+        tf.text = 'Find the bullet slide layout'
+
+        p = tf.add_paragraph()
+        p.text = 'Use _TextFrame.text for first bullet'
+        p.level = 1
+
+        p = tf.add_paragraph()
+        p.text = 'Use _TextFrame.add_paragraph() for subsequent bullets'
+        p.level = 2
+
